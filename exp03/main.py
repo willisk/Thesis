b19122fee6e47e42e38e4438f3b753193c594f58
Kurt Willis <willis@campus.tu-berlin.de>
2020-10-09 14:08:17 +0200

    cpu err

--- exp03/main.py
+++ exp03/main.py
@@ -200,13 +200,13 @@ for hp in utility.dict_product(hyperparameters):
                                           )
 
     # # dataset.plot(stats_net)
-    invert, target_labels = invert.cpu(), target_labels.cpu()
-    frames = dataset.plot_history(invert, target_labels)
+    target_labels = target_labels.cpu()
+    frames = dataset.plot_history(invert, target_labels
 
     if len(frames) > 1:  # animated gif
         for _ in range(10):  # make last frame stick
             frames.append(frames[-1])
-        anim = ArtistAnimation(plt.gcf(), frames,
+        anim=ArtistAnimation(plt.gcf(), frames,
                                interval=300, blit=True)
         plt.close()
         if args.save_images:
