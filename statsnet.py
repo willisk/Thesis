81b38872e0c2ce51a2c7011cd1bed652f632394d
Kurt Willis <willis@campus.tu-berlin.de>
2020-10-22 16:24:28 +0200

    change to bn stats

--- statsnet.py
+++ statsnet.py
@@ -83,19 +83,27 @@ class StatsHook(nn.Module):
 
                 nch = x.shape[1]
 
-                mean = x.mean([0, 2, 3])
-                var = x.var([0, 2, 3])
                 # permute(1, 0, 2, 3).contiguous().view(
                 #     [nch, -1]).var(1, unbiased=False)
 
-                if not self.state().class_conditional:
+                if self.state().class_conditional:
+                    # total_mean: [n_chan]
+                    # cc_mean: [10, n_chan]
+                    # m: [BS, n_chan]
+                    m, v = self.running_mean[labels], self.running_var[labels]
+                    mean = x.mean([0, 2, 3])
+                    var = x.var([0, 2, 3])
+                else:
+                    mean = x.mean([0, 2, 3])
+                    var = x.var([0, 2, 3])
                     if not hasattr(self, 'total_mean'):
-                        m, v = self.running_mean[labels], self.running_var[labels]
                         self.total_mean, self.total_var, _ = utility.reduce_mean_var(
                             self.running_mean, self.running_var, self.class_count)
                     m, v = self.total_mean, self.total_var
-                    self.regularization = (
-                        (x.mean([2, 3]) - m)**2 / v).sum(dim=1)
+                    if True:
+                        m, v = module.running_mean, module.running_var
+                    # self.regularization = (
+                    #     (x.mean([2, 3]) - m)**2 / v).sum(dim=1)
 
                 r_feature = torch.norm(v.data.type(var.type()) - var, 2) + torch.norm(
                     m.data.type(mean.type()) - mean, 2)
