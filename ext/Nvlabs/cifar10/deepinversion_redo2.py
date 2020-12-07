ae9015dd10ddd8fb43ed6c209065827b9519a9bc
Kurt Willis <willis@campus.tu-berlin.de>
2020-12-07 17:28:18 +0100

    git push

--- ext/Nvlabs/cifar10/deepinversion_redo2.py
+++ ext/Nvlabs/cifar10/deepinversion_redo2.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='PyTorch CIFAR10 DeepInversion')
     parser.add_argument('--bs', default=128, type=int, help='batch size')
-    parser.add_argument('--iters_mi', default=500, type=int,
+    parser.add_argument('--iters_mi', default=1000, type=int,
                         help='number of iterations for model inversion')
     parser.add_argument('--cig_scale', default=0.0,
                         type=float, help='competition score')
