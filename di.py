fc0465f22fa354c47f32b3320f49c7a62d29fce2
Kurt Willis <willis@campus.tu-berlin.de>
2020-12-21 19:31:04 +0100

    norm test

--- di.py
+++ di.py
@@ -271,10 +271,14 @@ def loss_stats(m_a, s_a, m_b, s_b):
     if isinstance(m_a, list):
         assert len(m_a) == len(m_b) and len(s_a) == len(s_b), \
             "lists need to be of same length"
-        loss_mean = sum(((ma - mb)**2).mean()
+        loss_mean = sum((ma - mb).norm(2)
                         for ma, mb in zip(m_a, m_b))  # / len(m_a)
-        loss_std = sum(((sa - sb)**2).mean()
+        loss_std = sum((sa - sb).norm(2)
                        for sa, sb in zip(s_a, s_b))  # / len(m_a)
+        # loss_mean = sum(((ma - mb)**2).mean()
+        #                 for ma, mb in zip(m_a, m_b))  # / len(m_a)
+        # loss_std = sum(((sa - sb)**2).mean()
+        #                for sa, sb in zip(s_a, s_b))  # / len(m_a)
         # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     else:
         loss_mean = ((m_a - m_b)**2).mean()
