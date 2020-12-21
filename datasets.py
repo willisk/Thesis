7d2e48c222673b8a3d3404883c7ee36d7dd4cdb7
Kurt Willis <willis@campus.tu-berlin.de>
2020-12-21 21:08:07 +0100

    renormalize

--- datasets.py
+++ datasets.py
@@ -60,7 +60,8 @@ class CIFAR10(Dataset):
     def __init__(self, load_data):
         transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010))
         ])
         if load_data:
             train_set = torchvision.datasets.CIFAR10(
