595964fa995ba2a94286e34b2e34335bf9a8eec2
Kurt Willis <willis@campus.tu-berlin.de>
2020-12-21 18:36:23 +0100

    test

--- ext/Nvlabs/cifar10/deepinversion-redo.py
+++ ext/Nvlabs/cifar10/deepinversion-redo.py
@@ -27,17 +27,7 @@ import sys
 import glob
 import collections
 
-# from resnet_cifar import ResNet34, ResNet18
-from ext.cifar10pretrained.cifar10_models import resnet34 as ResNet34
-
-try:
-    from apex.parallel import DistributedDataParallel as DDP
-    from apex import amp, optimizers
-    USE_APEX = True
-except ImportError:
-    print("Please install apex from https://www.github.com/nvidia/apex to run this example.")
-    print("will attempt to run without it")
-    USE_APEX = False
+USE_APEX = False
 
 PWD = os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))))
@@ -45,6 +35,9 @@ sys.path.append(PWD)
 import utility
 import importlib
 importlib.reload(utility)
+# from resnet_cifar import ResNet34, ResNet18
+
+from ext.cifar10pretrained.cifar10_models import resnet34 as ResNet34
 
 # provide intermeiate information
 debug_output = False
@@ -86,15 +79,62 @@ def jitter(x):
     return x
 
 
-def get_images(net, bs=256, epochs=1000, idx=-1, var_scale=0.00005,
-               prefix=None, competitive_scale=0.01, global_iteration=None,
-               optimizer=None, inputs=None, bn_reg_scale=0.0, random_labels=False,
-               device='cpu'):
+def loss_fn(data):
+    inputs, labels = data
+    outputs = net(inputs)
+    loss = 10 * sum(layer_losses)
+    loss += 0.001 * regularization(inputs)
+    loss += criterion(outputs, labels)
+    return loss
+
+
+def regularization(x):
+    diff1 = x[:, :, :, :-1] - x[:, :, :, 1:]
+    diff2 = x[:, :, :-1, :] - x[:, :, 1:, :]
+    diff3 = x[:, :, 1:, :-1] - x[:, :, :-1, 1:]
+    diff4 = x[:, :, :-1, :-1] - x[:, :, 1:, 1:]
+    loss_var = torch.norm(diff1) + torch.norm(diff2) + \
+        torch.norm(diff3) + torch.norm(diff4)
+    return loss_var
+
+
+if __name__ == "__main__":
+
+    bs = 256
+    iters = 400
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    print("loading resnet34")
+
+    net = ResNet34()
+    model_path = os.path.join(PWD, 'models/CIFAR10/net_resnet34.pt')
+    save_path, load_path = utility.save_load_path(model_path, True)
+    checkpoint = torch.load(load_path, map_location=device)
+
+    net.to(device)
+    net.eval()
+
+    criterion = nn.CrossEntropyLoss()
+
+    # place holder for inputs
+    inputs = torch.randn((256, 3, 32, 32),
+                         requires_grad=True, device=device)
+
+    optimizer = optim.Adam([inputs], lr=0.1)
+
+    net.load_state_dict(checkpoint)
+
+    batch_idx = 0
+    prefix = "runs/data_generation/" + "try1" + "/"
+
+    for create_folder in [prefix, prefix + "/best_images/"]:
+        if not os.path.exists(create_folder):
+            os.makedirs(create_folder)
+
+    print("Starting model inversion")
 
     best_cost = 1e6
-
-    inputs.data = torch.randn(
-        (bs, 3, 32, 32), requires_grad=True, device=device)
 
     criterion = nn.CrossEntropyLoss()
 
@@ -102,42 +142,38 @@ def get_images(net, bs=256, epochs=1000, idx=-1, var_scale=0.00005,
 
     targets = torch.LongTensor(range(bs)).to(device) % 10
 
-    # Create hooks for feature statistics catching
-    loss_r_feature_layers = []
+    net_layers = utility.get_bn_layers(net)
+    # layer_activations = [None] * len(net_layers)
+    layer_losses = [None] * len(net_layers)
+
+    def layer_hook_wrapper(idx):
+        def hook(module, inputs, outputs):
+            nch = inputs[0].shape[1]
+            mean = inputs[0].mean([0, 2, 3])
+            var = inputs[0].permute(1, 0, 2, 3).contiguous().view(
+                [nch, -1]).var(1, unbiased=False)
+            r_feature = torch.norm(module.running_var.data.type(var.type()) - var, 2) + torch.norm(
+                module.running_mean.data.type(var.type()) - mean, 2)
+            layer_losses[idx] = r_feature
+        return hook
+
+        # Create hooks for feature statistics catching
+    l = 0
     for module in net.modules():
         if isinstance(module, nn.BatchNorm2d):
-            loss_r_feature_layers.append(DeepInversionFeatureHook(module))
+            module.register_forward_hook(layer_hook_wrapper(l))
+            l += 1
+    # for l, layer in enumerate(net_layers):
+    #     layer.register_forward_hook(layer_hook_wrapper(l))
 
-    # setting up the range for jitter
-    lim_0, lim_1 = 2, 2
+    for epoch in range(iters):
+        inputs_jit = jitter(inputs)
 
-    for epoch in range(epochs):
-        # apply random jitter offsets
-        off1 = random.randint(-lim_0, lim_0)
-        off2 = random.randint(-lim_1, lim_1)
-        inputs_jit = torch.roll(inputs, shifts=(off1, off2), dims=(2, 3))
-
-        # foward with jit images
         optimizer.zero_grad()
-        outputs = net(inputs_jit)
-        loss = criterion(outputs, targets)
-        loss_target = loss.item()
-
-        # apply total variation regularization
-        diff1 = inputs_jit[:, :, :, :-1] - inputs_jit[:, :, :, 1:]
-        diff2 = inputs_jit[:, :, :-1, :] - inputs_jit[:, :, 1:, :]
-        diff3 = inputs_jit[:, :, 1:, :-1] - inputs_jit[:, :, :-1, 1:]
-        diff4 = inputs_jit[:, :, :-1, :-1] - inputs_jit[:, :, 1:, 1:]
-        loss_var = torch.norm(diff1) + torch.norm(diff2) + \
-            torch.norm(diff3) + torch.norm(diff4)
-        loss = loss + var_scale * loss_var
-
-        # R_feature loss
-        loss_distr = sum([mod.r_feature for mod in loss_r_feature_layers])
-        loss = loss + bn_reg_scale * loss_distr  # best for noise before BN
+        loss = loss_fn((inputs, targets))
 
         if debug_output and epoch % 200 == 0:
-            print(f"It {epoch}\t Losses: total: {loss.item():3.3f},\ttarget: {loss_target:3.3f} \tR_feature_loss unscaled:\t {loss_distr.item():3.3f}")
+            print(f"It {epoch}\t Losses: total: {loss.item():3.3f}")
             vutils.save_image(inputs.data.clone(),
                               './{}/output_{}.png'.format(prefix,
                                                           epoch // 200),
@@ -155,10 +191,6 @@ def get_images(net, bs=256, epochs=1000, idx=-1, var_scale=0.00005,
     outputs = net(best_inputs)
     _, predicted_teach = outputs.max(1)
 
-    if idx == 0:
-        print('Teacher correct out of {}: {}, loss at {}'.format(
-            bs, predicted_teach.eq(targets).sum().item(), criterion(outputs, targets).item()))
-
     name_use = "best_images"
     if prefix is not None:
         name_use = prefix + name_use
@@ -167,67 +199,3 @@ def get_images(net, bs=256, epochs=1000, idx=-1, var_scale=0.00005,
     vutils.save_image(best_inputs[:20].clone(),
                       './{}/output_{}.png'.format(name_use, next_batch),
                       normalize=True, scale_each=True, nrow=10)
-
-    return best_inputs
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        description='PyTorch CIFAR10 DeepInversion')
-    parser.add_argument('--bs', default=256, type=int, help='batch size')
-    parser.add_argument('--iters_mi', default=2000, type=int,
-                        help='number of iterations for model inversion')
-    parser.add_argument('--cig_scale', default=0.0,
-                        type=float, help='competition score')
-    parser.add_argument('--di_lr', default=0.1, type=float,
-                        help='lr for deep inversion')
-    parser.add_argument('--di_var_scale', default=0.001,
-                        type=float, help='TV L2 regularization coefficient')
-    parser.add_argument('--r_feature_weight', default=10,
-                        type=float, help='weight for BN regularization statistic')
-    parser.add_argument('--exp_descr', default="try1",
-                        type=str, help='name to be added to experiment name')
-
-    args = parser.parse_args([])
-
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    print("loading resnet34")
-
-    net = ResNet34()
-    model_path = os.path.join(PWD, 'models/CIFAR10/net_resnet34.pt')
-    save_path, load_path = utility.save_load_path(model_path, True)
-    checkpoint = torch.load(load_path, map_location=device)
-
-    net.to(device)
-    net.eval()
-
-    criterion = nn.CrossEntropyLoss()
-
-    # place holder for inputs
-    data_type = torch.float
-    inputs = torch.randn((args.bs, 3, 32, 32),
-                         requires_grad=True, device=device, dtype=data_type)
-
-    optimizer_di = optim.Adam([inputs], lr=args.di_lr)
-
-    net.load_state_dict(checkpoint)
-
-    batch_idx = 0
-    prefix = "runs/data_generation/" + args.exp_descr + "/"
-
-    for create_folder in [prefix, prefix + "/best_images/"]:
-        if not os.path.exists(create_folder):
-            os.makedirs(create_folder)
-
-    global_iteration = 0
-
-    print("Starting model inversion")
-
-    inputs = get_images(net=net, bs=args.bs, epochs=args.iters_mi, idx=batch_idx,
-                        prefix=prefix,
-                        global_iteration=global_iteration,
-                        optimizer=optimizer_di, inputs=inputs, bn_reg_scale=args.r_feature_weight,
-                        var_scale=args.di_var_scale, random_labels=False,
-                        device=device)
