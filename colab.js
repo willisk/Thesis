08ab19b9399206f6bad7b25983fa2b1668d44647
Kurt Willis <willis@campus.tu-berlin.de>
2021-01-05 13:37:09 +0100

    upd show batch

---
+++ colab.js
@@ -0,0 +1,23 @@
+var startClickConnect = function startClickConnect() {
+    var clickConnect = function clickConnect() {
+        console.log("Connnect Clicked");
+        document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click();
+        document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click();
+    };
+
+    click_interval = 300000;
+    var intervalId = setInterval(clickConnect, click_interval);
+    var intervalId2;
+
+    setTimeout(() => { intervalId2 = setInterval(clickConnect, click_interval); }, 1000);
+
+    var stopClickConnectHandler = function stopClickConnect() {
+        clearInterval(intervalId);
+        clearInterval(intervalId2);
+        console.log("Stopped");
+    };
+
+    return stopClickConnectHandler;
+};
+
+var stopClickConnect = startClickConnect();
