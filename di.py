8839fa5bdae95e8ff75c62e4cdbf9506436c1642
Kurt Willis <willis@campus.tu-berlin.de>
2021-01-06 01:25:28 +0100

    rm relu

--- di.py
+++ di.py
@@ -462,16 +462,16 @@ methods = [
         project=project_RP_CC,
         class_conditional=True,
     ),
-    loss_fn_wrapper(
-        name="RP ReLU",
-        project=project_RP_relu,
-        class_conditional=False,
-    ),
-    loss_fn_wrapper(
-        name="RP ReLU CC",
-        project=project_RP_relu_CC,
-        class_conditional=True,
-    ),
+    # loss_fn_wrapper(
+    #     name="RP ReLU",
+    #     project=project_RP_relu,
+    #     class_conditional=False,
+    # ),
+    # loss_fn_wrapper(
+    #     name="RP ReLU CC",
+    #     project=project_RP_relu_CC,
+    #     class_conditional=True,
+    # ),
     loss_fn_wrapper(
         name="NN ALL + RP CC",
         project=combine(project_NN_all, project_RP_CC),
