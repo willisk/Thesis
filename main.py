4c24d80c75d73f77d040714230bbde63063b0a53
Kurt Willis <willis@campus.tu-berlin.de>
2021-01-09 12:53:41 +0100

    nn cc

--- main.py
+++ main.py
@@ -441,52 +441,52 @@ def criterion_only(data):
 
 
 methods = [
-    ("CRITERION", criterion_only),
-    loss_fn_wrapper(
-        name="NN",
-        project=project_NN,
-        class_conditional=False,
-    ),
+    # ("CRITERION", criterion_only),
+    # loss_fn_wrapper(
+    #     name="NN",
+    #     project=project_NN,
+    #     class_conditional=False,
+    # ),
     loss_fn_wrapper(
         name="NN CC",
         project=project_NN,
         class_conditional=True,
     ),
-    loss_fn_wrapper(
-        name="NN ALL",
-        project=project_NN_all,
-        class_conditional=False,
-    ),
-    loss_fn_wrapper(
-        name="NN ALL CC",
-        project=project_NN_all,
-        class_conditional=True,
-    ),
-    loss_fn_wrapper(
-        name="RP",
-        project=project_RP,
-        class_conditional=False,
-    ),
-    loss_fn_wrapper(
-        name="RP CC",
-        project=project_RP_CC,
-        class_conditional=True,
-    ),
     # loss_fn_wrapper(
-    #     name="RP ReLU",
-    #     project=project_RP_relu,
+    #     name="NN ALL",
+    #     project=project_NN_all,
     #     class_conditional=False,
     # ),
     # loss_fn_wrapper(
-    #     name="RP ReLU CC",
-    #     project=project_RP_relu_CC,
+    #     name="NN ALL CC",
+    #     project=project_NN_all,
     #     class_conditional=True,
     # ),
-    loss_fn_wrapper(
-        name="NN ALL + RP CC",
-        project=combine(project_NN_all, project_RP_CC),
-        class_conditional=True,
-    ),
+    # loss_fn_wrapper(
+    #     name="RP",
+    #     project=project_RP,
+    #     class_conditional=False,
+    # ),
+    # loss_fn_wrapper(
+    #     name="RP CC",
+    #     project=project_RP_CC,
+    #     class_conditional=True,
+    # ),
+    # # loss_fn_wrapper(
+    # #     name="RP ReLU",
+    # #     project=project_RP_relu,
+    # #     class_conditional=False,
+    # # ),
+    # # loss_fn_wrapper(
+    # #     name="RP ReLU CC",
+    # #     project=project_RP_relu_CC,
+    # #     class_conditional=True,
+    # # ),
+    # loss_fn_wrapper(
+    #     name="NN ALL + RP CC",
+    #     project=combine(project_NN_all, project_RP_CC),
+    #     class_conditional=True,
+    # ),
 ]
 
 # ======= Distortation =======
