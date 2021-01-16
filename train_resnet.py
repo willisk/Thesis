c09b9b4f5b7a6c43b8777cf4fbdd3880ffef4d58
Kurt Willis <willis@campus.tu-berlin.de>
2021-01-16 13:00:24 +0100

    train resnet

---
+++ train_resnet.py
@@ -0,0 +1,191 @@
+"""Training Resnet"""
+import os
+import sys
+
+import random
+
+import argparse
+from collections import defaultdict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torchvision
+
+import matplotlib.pyplot as plt
+# plt.style.use('default')
+
+import numpy as np
+
+PWD = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PWD)
+
+import utility
+import inversion
+import datasets
+import debug
+import nets
+
+try:
+    get_ipython()   # pylint: disable=undefined-variable
+    interactive_notebook = True
+except:
+    interactive_notebook = False
+
+
+if interactive_notebook:
+    import importlib
+    importlib.reload(utility)
+    importlib.reload(inversion)
+    importlib.reload(datasets)
+    importlib.reload(debug)
+    importlib.reload(nets)
+
+from debug import debug
+
+
+# ======= Arg Parse =======
+parser = argparse.ArgumentParser(description="Reconstruction Tests")
+parser.add_argument(
+    "-dataset", choices=['CIFAR10', 'MNIST'], required=True)
+parser.add_argument("-seed", type=int, default=0)
+parser.add_argument("-nn_lr", type=float, default=0.01)
+parser.add_argument("-nn_steps", type=int, default=100)
+parser.add_argument("-batch_size", type=int, default=64)
+parser.add_argument("-n_random_projections", type=int, default=256)
+parser.add_argument("-inv_lr", type=float, default=0.1)
+parser.add_argument("-inv_steps", type=int, default=100)
+parser.add_argument("-f_reg", type=float, default=0.001)
+parser.add_argument("-f_crit", type=float, default=1)
+parser.add_argument("-f_stats", type=float, default=10)
+parser.add_argument("-size_A", type=int, default=-1)
+parser.add_argument("-size_B", type=int, default=64)
+parser.add_argument("-show_after", type=int, default=50)
+parser.add_argument("--nn_resume_train", action="store_true")
+parser.add_argument("--nn_reset_train", action="store_true")
+parser.add_argument("--use_amp", action="store_true")
+parser.add_argument("--use_std", action="store_true")
+parser.add_argument("--use_jitter", action="store_true")
+parser.add_argument("--plot_ideal", action="store_true")
+parser.add_argument("--scale_each", action="store_true")
+parser.add_argument("--reset_stats", action="store_true")
+
+if 'ipykernel_launcher' in sys.argv[0]:
+    # args = parser.parse_args('-dataset MNIST'.split())
+    args = parser.parse_args('-dataset CIFAR10'.split())
+    args.inv_steps = 2
+    args.size_A = 16
+    args.size_B = 8
+    args.batch_size = 8
+else:
+    args = parser.parse_args()
+
+USE_DRIVE = True
+
+print("#", __doc__)
+print("# on", args.dataset)
+
+
+# ======= Hyperparameters =======
+print("Hyperparameters:")
+print(utility.dict_to_str(vars(args), '\n'), '\n')
+
+# ======= Set Seeds =======
+
+
+def set_seed():
+    if args.seed == -1:
+        return
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    os.environ['PYTHONHASHSEED'] = str(args.seed)
+
+
+set_seed()
+
+
+# Neural Network
+nn_lr = args.nn_lr
+nn_steps = args.nn_steps
+nn_resume_training = args.nn_resume_train
+nn_reset_training = args.nn_reset_train
+
+# Random Projections
+n_random_projections = args.n_random_projections
+
+# Inversion
+inv_lr = args.inv_lr
+inv_steps = args.inv_steps
+
+# ======= Device =======
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Running on '{DEVICE}'\n")
+
+# ======= Create Dataset =======
+
+if args.dataset == 'CIFAR10':
+    dataset = datasets.CIFAR10()
+elif args.dataset == 'MNIST':
+    dataset = datasets.MNIST()
+
+MODELDIR = dataset.data_dir
+
+A, B, B_val = dataset.get_datasets(size_A=args.size_A, size_B=args.size_B)
+
+
+DATA_A = utility.DataL(
+    A, batch_size=args.batch_size, shuffle=True, device=DEVICE)
+if args.plot_ideal:
+    test_data = utility.DataL(
+        B, batch_size=-1, shuffle=True, device=DEVICE)
+    ideal_data = next(iter(test_data))
+
+input_shape = dataset.input_shape
+n_dims = dataset.n_dims
+n_classes = dataset.n_classes
+
+# ======= Setup Methods =======
+STD = args.use_std
+stats_path = os.path.join(MODELDIR, "stats_{}.pt")
+
+# ======= Neural Network =======
+model_path, net = dataset.net()
+net.to(DEVICE)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=nn_lr)
+# utility.train(net, DATA_A, criterion, optimizer,
+#               model_path=model_path,
+#               epochs=nn_steps,
+#               resume_training=nn_resume_training,
+#               reset=nn_reset_training,
+#               plot=True,
+#               use_drive=USE_DRIVE,
+#               )
+# net.eval()
+
+if not 'ipykernel_launcher' in sys.argv[0]:
+    utility.print_net_accuracy(net, DATA_A)
+
+verifier_path, verifier_net = dataset.verifier_net()
+if verifier_net:
+    verifier_net.to(DEVICE)
+    optimizer = torch.optim.Adam(verifier_net.parameters(), lr=nn_lr)
+    utility.train(verifier_net, DATA_A, criterion, optimizer,
+                  model_path=verifier_path,
+                  epochs=nn_steps,
+                  resume_training=nn_resume_training,
+                  reset=nn_reset_training,
+                  use_drive=USE_DRIVE,
+                  )
+    if not 'ipykernel_launcher' in sys.argv[0]:
+        print("verifier ", end='')
+        utility.print_net_accuracy(verifier_net, DATA_A)
+print()
